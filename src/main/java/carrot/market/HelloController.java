d437a0eeeb7e2f363bb67c89930494bc286bdc2d
killerwhale1125 <killerwhale1125@naver.com>
2024-07-12 13:56:06 +0900

    Config 설정 제외

---
+++ src/main/java/carrot/market/HelloController.java
@@ -0,0 +1,15 @@
+package carrot.market;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello World!";
+    }
+}
