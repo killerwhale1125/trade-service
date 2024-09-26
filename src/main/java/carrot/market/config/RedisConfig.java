ead1c09f7080b23d22c6400e705161702e6b060c
killerwhale1125 <killerwhale1125@naver.com>
2024-09-26 13:07:16 +0900

    JPA, Redis @Transactional 로 동시에 관리하게 설정

--- src/main/java/carrot/market/config/RedisConfig.java
+++ src/main/java/carrot/market/config/RedisConfig.java
@@ -9,9 +9,12 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableRedisHttpSession
@@ -30,13 +33,27 @@ public class RedisConfig {
     }
 
     /** RedisTemplate은 Redis에 데이터를 저장하고 읽는 데 사용 **/
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(@Qualifier("redisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(@Qualifier("redisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//
+//        return redisTemplate;
+//    }
 
-        return redisTemplate;
+    // RedisTemplate 설정
+    @Bean
+    public StringRedisTemplate redisTemplate() {
+        StringRedisTemplate template = new StringRedisTemplate(redisConnectionFactory());
+        template.setEnableTransactionSupport(true); // Redis 트랜잭션 지원 활성화
+        return template;
+    }
+
+    // JPA 트랜잭션 매니저 설정
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
     }
 }
